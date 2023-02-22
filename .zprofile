246c7ba19d652018606260ba7033cf1c00b8991b
Sourav <sourav@pop-os.localdomain>
2023-02-22 09:56:12 +0530

    Update neovim config

---
+++ .zprofile
@@ -0,0 +1,7 @@
+# Fig pre block. Keep at the top of this file.
+[[ -f "$HOME/.fig/shell/zprofile.pre.zsh" ]] && builtin source "$HOME/.fig/shell/zprofile.pre.zsh"
+# Set PATH, MANPATH, etc., for Homebrew.
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+# Fig post block. Keep at the bottom of this file.
+[[ -f "$HOME/.fig/shell/zprofile.post.zsh" ]] && builtin source "$HOME/.fig/shell/zprofile.post.zsh"
---
+++ nvim/lua/configs/treesitter.lua
@@ -0,0 +1,14 @@
+require("nvim-treesitter.configs").setup(
+  astronvim.user_plugin_opts("plugins.treesitter", {
+    highlight = { enable = true, additional_vim_regex_highlighting = false },
+    t_commentstring = { enable = true, enable_autocmd = false },
+    rainbow = {
+      enable = true,
+      -- disable = { "html" },
+      extended_mode = false,
+      max_file_lines = nil
+    },
+    autotag = { enable = true },
+    incremental_selection = { enable = true },
+    indent = { enable = false }
+  }))
